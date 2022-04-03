138dc0d1cb794c5523af532efddba4f058dc55ea
Jim Klein <j.mesklein@gmail.com>
2022-04-03 14:39:06 -0700

    budget changes

--- libs/project-dashboard/src/lib/project-dashboard.module.ts
+++ libs/project-dashboard/src/lib/project-dashboard.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardShellComponent } from './dashboard-shell/dashboard-shell.component';
+import { BudgetSummaryComponent } from './budget-summary/budget-summary.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    // DashboardShellComponent,
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
   ],
+  declarations: [
+    DashboardShellComponent,
+    BudgetSummaryComponent
+  ],
+  exports:  [
+    DashboardShellComponent,
+  ]
 })
 export class ProjectDashboardModule {}
